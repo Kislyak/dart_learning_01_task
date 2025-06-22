2f7938054ae1dc1efc8ee4073585c57a517257e3
Kysliak Andrii <Kislyak.work@gmail.com>
2025-06-22 22:12:51 +0300

    feat(DART-06): initial commit

---
+++ 1_task.dart
@@ -0,0 +1,7 @@
+void main() {
+  print("Hello, World!");
+  print("This is my first Dart program.");
+  print("I am learning Dart programming.");
+  print("Dart is a great language for building web and mobile apps.");
+  print("I am excited to learn more about Dart.");
+}
