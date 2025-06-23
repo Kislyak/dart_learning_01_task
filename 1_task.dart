cfba6264151df8e890e9438b7e1d223742baadff
Kysliak Andrii <Kislyak.work@gmail.com>
2025-06-23 19:37:45 +0300

    Завдання 2: Оператори порівняння

--- 1_task.dart
+++ 1_task.dart
@@ -1,11 +1,29 @@
 void main() {
-  // Завдання 1: Математичні оператори
-  double height = 30.9;
-  double width = 18.4;
-  double area = height * width;
-  double perimeter = 2 * (height + width);
-  int fullSquares = (height ~/ 1) * (width ~/ 1);
-  print('Area: $area');
-  print('Perimeter: $perimeter');
-  print('Full squares: $fullSquares');
+  // // Завдання 1: Математичні оператори
+  // double height = 30.9;
+  // double width = 18.4;
+  // double area = height * width;
+  // double perimeter = 2 * (height + width);
+  // int fullSquares = (height ~/ 1) * (width ~/ 1);
+  // print('Area: $area');
+  // print('Perimeter: $perimeter');
+  // print('Full squares: $fullSquares');
+
+  //Завдання 2: Оператори порівняння
+  double number = 55.0;
+  if (number > 50) {
+    print('Number bigger than 50');
+  } else {
+    print('Number less than or equal to 50');
+  }
+  if (number < 100) {
+    print('Number less than 100');
+  } else {
+    print('Number bigger than or equal to 100');
+  }
+  if (number % 5 == 0) {
+    print('Number is divisible by 5');
+  } else {
+    print('Number is not divisible by 5');
+  }
 }
